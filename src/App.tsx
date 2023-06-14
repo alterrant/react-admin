e10b71f2252b009fef37b27b063ce849e300f288
alterrant <pandor246@mail.ru>
2023-06-14 18:21:27 +0300

    style(*): add linters

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      React-admin
-    </div>
-  );
+  return <div className="App">React-admin</div>;
 }
 
 export default App;
